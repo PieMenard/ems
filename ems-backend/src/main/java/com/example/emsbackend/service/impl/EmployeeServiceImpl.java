a762f720855fa11dca86bedd7cf3c2cb74fdf196
PieMenard <110061246+PieMenard@users.noreply.github.com>
2024-04-19 16:17:10 -0400

    build get all employees REST API

--- ems-backend/src/main/java/com/example/emsbackend/service/impl/EmployeeServiceImpl.java
+++ ems-backend/src/main/java/com/example/emsbackend/service/impl/EmployeeServiceImpl.java
@@ -9,6 +9,9 @@ import com.example.emsbackend.service.EmployeeService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 @AllArgsConstructor
 public class EmployeeServiceImpl implements EmployeeService {
@@ -27,5 +30,12 @@ public class EmployeeServiceImpl implements EmployeeService {
         Employee employee = employeeRepository.findById(employeeId)
                 .orElseThrow(()-> new ResourceNotFoundException("Not found employee with id:" + employeeId));
         return EmployeeMapper.mapToEmployeeDto(employee);
+    }
+
+    @Override
+    public List<EmployeeDto> getAllEmployees() {
+        List<Employee> employees = employeeRepository.findAll();
+        return employees.stream().map(EmployeeMapper::mapToEmployeeDto)
+                .collect(Collectors.toList());
     }
 }
